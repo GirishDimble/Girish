c9b547f4afb19c2e9a2d5e53326664a4208c28f5
Rahul Belgaonkar <58828810+Rahuleus12@users.noreply.github.com>
2024-11-09 12:04:02 +0530

    Update Cals.py

--- Cals.py
+++ Cals.py
@@ -21,22 +21,16 @@ def mul(num1, num2):
     result = num1 * num2
     return jsonify(result=result)
 
-##Devision
-@app.route('/div/<float:num1>/<float:num2>', methods=['GET'])
-def div(num3, num2):
-    result = num3/num2
-    return jsonify(result=result)
-
-##Remainder
-@app.route('/div/<float:num1>/<float:num2>', methods=['GET'])
-def div(num3, num2):
-    result = num3%num2
-    return jsonify(result=result)
-
 ##Power
 @app.route('/div/<float:num1>/<float:num2>', methods=['GET'])
 def div(num3, num2):
     result = num3**num2
+    return jsonify(result=result)
+
+##Devision
+@app.route('/div/<float:num1>/<float:num2>', methods=['GET'])
+def div(num3, num2):
+    result = num3/num2
     return jsonify(result=result)
  
 if __name__ == '__main__':
